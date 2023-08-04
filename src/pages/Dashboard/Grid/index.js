1c736a24e414011ad456429aa529f2ccea83a494
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-08-05 01:28:43 +0530

    Update index.js

--- src/pages/Dashboard/Grid/index.js
+++ src/pages/Dashboard/Grid/index.js
@@ -4,25 +4,72 @@ import { motion } from "framer-motion"
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import { NavLink } from 'react-router-dom'
 import StarBorderIcon from '@mui/icons-material/StarBorder';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 // SINLGLE COIN DATA RECIEVE AS A PROP
-function Grid({ coin }) {
+function Grid({ coin , refresh }) {
 
 
-const [add , setadd] = useState(false);
+    const [add, setadd] = useState(false);
 
-    var existingArray = JSON.parse(localStorage.getItem('myArray')) || [];
-    function handleWatchlist(event){
-        event.preventDefault();
-        var item = coin.id 
-        existingArray.push(item);
-        localStorage.setItem('myArray', JSON.stringify(existingArray));
-        setadd(true);
+    function background(){
+        var existingArray = JSON.parse(localStorage.getItem('myArray'));
+        existingArray.map((value) => {  
+            if (value == item) {
+               setadd(true);
+            }});
+    }
+    useEffect(()=>{
+        background();
+    },[])
+    
+    var item = coin.id;
+    
+    function handleWatchlist(event) {
+        event.preventDefault()
+        var existingArray = JSON.parse(localStorage.getItem('myArray')) || [];
+
+        if (existingArray.length > 0) {
+            var count = 0;
+            existingArray.map((value) => {  
+              
+                if (value == item) {
+                    count++;
+
+                    var newArray = existingArray.filter(crypto => crypto !==item);
+                    console.log(newArray);
+                    localStorage.setItem('myArray', JSON.stringify(newArray));
+                    toast.error("Crypto Coin Removed from Wacthlist!")
+                    refresh();
+                }
+            })
+            if(count==0){
+            existingArray.push(item);
+            localStorage.setItem('myArray', JSON.stringify(existingArray))
+            console.log("wow>>",existingArray);
+            setadd(true)
+            toast.success("Crypto Coin Added To Wacthlist!")
+           
+        }
+        else{
+                    
+                }
+            
+            }
+            else {
+                existingArray.push(item)
+                localStorage.setItem('myArray', JSON.stringify(existingArray))
+                console.log(existingArray);
+                setadd(true)
+                toast.success("Crypto Coin Added To Wacthlist!")
+               
+        }
     }
 
     return (
         // routing in coins 
-        <NavLink to={`/coin/${coin.id}`} className="grid-link"> 
+        <NavLink to={`/coin/${coin.id}`} className="grid-link">
             {/* // Condition adding classs to the main div of coin */}
             <motion.div
                 whileHover={{ scale: 1.04 }}
@@ -31,11 +78,11 @@ const [add , setadd] = useState(false);
                 <div className='coin-info-div' >
                     <img className='coin-img' src={coin.image}></img>
                     <div className='coin-symbol-name'>
-                        <p className='coin-symbol'>{coin.symbol}-INR</p>
+                        <p className='coin-symbol'>{coin.symbol}</p>
                         <p className='coin-name'>{coin.name}</p>
                     </div>
                     <div className='watchlist-button'>
-                         { coin.price_change_percentage_24h > 0 ?   <StarBorderIcon className={`chip-up ${add ? "active-green" : "" }`} onClick={(event) => handleWatchlist(event)} /> : <StarBorderIcon onClick={(event) => handleWatchlist(event)} className= {`chip-down ${add ? "active-red" : ""} `} />}
+                        {coin.price_change_percentage_24h > 0 ? <StarBorderIcon className={`chip-up ${add == true ? "active-green" : ""}`} onClick={(event) => handleWatchlist(event)} /> : <StarBorderIcon onClick={(event) => handleWatchlist(event)} className={`chip-down ${add == true ? "active-red" : ""} `} />}
                     </div>
                 </div>
                 {/* condition rendoring style of coins green and red style theme */}
@@ -44,7 +91,7 @@ const [add , setadd] = useState(false);
                         <div className='chip-div'>
                             <div className='chip-green'>{coin.price_change_percentage_24h.toFixed(2)}%</div>
                             <TrendingUpIcon className='chip-up' />
-                
+
 
                         </div>
                     )
@@ -55,7 +102,7 @@ const [add , setadd] = useState(false);
                         </div>
                     )
                 }
-                
+
                 {/* condition price color  */}
                 <div>
                     <h2 className='price'
@@ -76,3 +123,4 @@ const [add , setadd] = useState(false);
 }
 
 export default Grid
+
